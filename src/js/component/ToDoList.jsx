0320659607df3525743c41d0ffdcbef991e8557a
dcolas98 <dcolas98@gmail.com>
2022-07-06 17:07:12 +0000

    1st commit

---
+++ src/js/component/ToDoList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react';
+import ToDoForm from './ToDoForm.jsx';
+import ToDoItem from './ToDoItem.jsx';
+
+export const ToDoList = () => {
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = todo => {
+		const newTodos = [todo, ...todos];
+		setTodos(newTodos)
+	};
+
+	const removeTodo = id => {
+		const removeArr = [...todos].filter(todo => todo.id !== id)
+		setTodos(removeArr)
+	}
+
+	const completeTodo = id => {
+		let updatedTodos = todos.map(todo => {
+			if (todo.id === id) {
+				todo.isComplete = !todo.isComplete
+			}
+			return todo;
+		})
+		setTodos(updatedTodos);
+	}
+  
+	return (
+	<div className="todo-container">
+		<h1>To Do List</h1>
+		<ToDoForm onSubmit={addTodo} />
+		<ToDoItem 
+		todos={todos}
+		completeTodo={completeTodo} removeTodo={removeTodo} />
+	</div>
+  );
+}
+
+
+export default ToDoList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+// import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
+
+// //create your first component
+// const Home = () => {
+// 	return (
+// 		<div>
+// 			<h1 className="mt-5">Todos</h1>
+//     <ul>
+//         <li>Make bed</li>
+//         <li>Wash my hands</li>
+//         <li>Eat</li>
+// 		<li>Walk the dog</li>
+//     </ul>
+// 		</div>
+// 	);
+// };
+
+// function addTd = () => {
+// 	return ( 
+		
+// 	)
+// }
+
+// export default Home;
+
+
+import React from "react";
+import ToDoList from "./ToDoList.jsx";
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<ToDoList />
+		</>
 	);
 };
 
